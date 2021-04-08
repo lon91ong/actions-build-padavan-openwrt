d04a5e0d3f60dfcc7c0ead98d8498596d7ecb358
lon91ong <lon91ong@gmail.com>
2021-04-08 15:09:13 +0800

    Update public.sh

--- padavan/public/public.sh
+++ padavan/public/public.sh
@@ -4,11 +4,13 @@
 
 user_name="admin"                        # 用户名 admin
 user_password=$user_name                 # 用户密码 admin
-lan_ip="192.168.77"                       # lan 地址 192.168.3.1 一定别写后面的 .1
+lan_ip="192.168.77"                      # lan 地址 192.168.3.1 一定别写后面的 .1
 wifi_password="1234567890"               # wifi密码，切记密码最少8位 admin
 version_time=$(date "+%Y%m%d")           # 自动时间更新时版本号: 20200320
 default_path="./user/shared/defaults.h"  # 默认文件配置目录
 
+rm -rf ./user
+svn co "https://github.com/chongshengB/rt-n56u/trunk/trunk/user" ./user
 
 echo '修改用户名'
 sed -i 's/#define\s*SYS_USER_ROOT\s*"admin"/#define  SYS_USER_ROOT     "'$user_name'"/g' $default_path
@@ -24,6 +26,7 @@ sed -i "s/1234567890/$wifi_password/g" $default_path
 
 echo "更新版本号时间"
 sed -i "s/FIRMWARE_BUILDS_REV=[0-9]*/FIRMWARE_BUILDS_REV=$version_time/g" ./versions.inc
+
 
 # 2. 删除预设项
 ################################################################################################
